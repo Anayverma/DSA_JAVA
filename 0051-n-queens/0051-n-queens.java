f92e1829fc1a870162f7563468b9214fbe78df2a
Anay Verma <127543533+Anayverma@users.noreply.github.com>
2024-07-26 14:39:08 +0530

    Time: 2 ms (88.77%), Space: 45.1 MB (8.75%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,62 +1,65 @@
-import java.util.*;
 class Solution {
-    public static List < List < String >> solveNQueens(int n) {
-        char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                board[i][j] = '.';
-        List < List < String >> res = new ArrayList < List < String >> ();
-        int leftRow[] = new int[n];
-        int upperDiagonal[] = new int[2 * n - 1];
-        int lowerDiagonal[] = new int[2 * n - 1];
-        solve(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
-        return res;
+    List<List<String>> result = new ArrayList<>();
+    
+    public List<List<String>> solveNQueens(int n) {
+        char[][] cb = new char[n][n]; // chessboard
+        
+        // Initialize chessboard with '.'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cb[i][j] = '.';
+            }
+        }
+        
+        helper(n, 0, cb);
+        
+        return result;
     }
-
-
-
-    static void solve(int col, char[][] board, List < List < String >> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
-        if (col == board.length) {
-            res.add(construct(board));
+    
+    public boolean isSafe(int n, int row, int col,char [][] cb) {
+        // Check column on this row
+        for (int i = 0; i < col; i++) {
+            if (cb[row][i] == 'Q') {
+                return false;
+            }
+        }
+        
+        // Check upper left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (cb[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        // Check lower left diagonal
+        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
+            if (cb[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public void helper(int n, int col,  char[][] cb) {
+        if (col == n) {
+            // Found a solution, add to result
+            List<String> solution = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                solution.add(String.valueOf(cb[i]));
+            }
+            result.add(solution);
             return;
         }
-
-        for (int row = 0; row < board.length; row++) {
-            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[board.length - 1 + col - row] == 0) {
-                board[row][col] = 'Q';
-                leftRow[row] = 1;
-                lowerDiagonal[row + col] = 1;
-                upperDiagonal[board.length - 1 + col - row] = 1;
-                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDiagonal);
-                board[row][col] = '.';
-                leftRow[row] = 0;
-                lowerDiagonal[row + col] = 0;
-                upperDiagonal[board.length - 1 + col - row] = 0;
-            }
+        
+        for (int i = 0; i < n; i++) 
+            if (isSafe(n, i, col,cb)) {
+                cb[i][col] = 'Q'; // Place Queen
+                
+                helper(n, col + 1, cb);
+                
+                cb[i][col] = '.'; // Backtrack
         }
     }
 
-
-    static List < String > construct(char[][] board) {
-        List < String > res = new LinkedList < String > ();
-        for (int i = 0; i < board.length; i++) {
-            String s = new String(board[i]);
-            res.add(s);
-        }
-        return res;
-    }
-    public static void main(String args[]) {
-        int N = 4;
-        List < List < String >> queen = solveNQueens(N);
-        int i = 1;
-        for (List < String > it: queen) {
-            System.out.println("Arrangement " + i);
-            for (String s: it) {
-                System.out.println(s);
-            }
-            System.out.println();
-            i += 1;
-        }
-
-    }
 }
