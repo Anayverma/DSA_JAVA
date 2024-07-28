e112e5b96fc71e1073e477cc8cb730fc3238fbe7
Anay Verma <127543533+Anayverma@users.noreply.github.com>
2024-07-28 23:37:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,29 +1,16 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            }
-        }
-
-        return -1;
+        int left=0,right=nums.length-1;
+      while(left<right){
+        int mid=(left+right)/2;
+        System.out.println(mid+"  l"+left+"  R"+right);
+        if(target == nums[mid]) return mid;
+        else if(target>=nums[left] && target<nums[mid]) right=mid-1;
+        else if(target <=nums[right] && target>nums[mid]) left= mid+1;
+        else if( target > nums[right] ) right= mid-1;
+        else left= mid+1;
+      }  if(left>=0&& left<nums.length&&nums[left]==target)return left;
+      if(right >=0&&right <nums.length&&nums[right]== target) return right;
+      return -1;
     }
 }
