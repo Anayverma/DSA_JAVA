9f83cee91c3efe18fcc2a822b39a9f92bfd53e5c
Anay Verma <127543533+Anayverma@users.noreply.github.com>
2024-08-06 22:22:11 +0530

    Time: 1 ms (99.55%), Space: 56.8 MB (67.58%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,25 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // Initialize sum with the first element
-        // This will also handle the case when the array has only one element
-        int sum = nums[0];
-        int maxsum = sum;
-
-        // Start the loop from the second element
-        for (int i = 1; i < nums.length; i++) {
-            // If the current sum is negative, reset it to the current element
-            if (sum < 0) {
-                sum = nums[i];
-            } else {
-                // Otherwise, add the current element to the sum
-                sum += nums[i];
-            }
-            
-            // Update maxsum if the current sum is greater than maxsum
-            if (sum > maxsum) {
-                maxsum = sum;
-            }
+        int sum=nums[0];
+        int max=sum;
+        for(int i=1;i<nums.length;i++){
+            sum=Math.max(sum+nums[i],nums[i]);
+            max=Math.max(max,sum);
         }
-        return maxsum;
+        return max;
     }
 }
