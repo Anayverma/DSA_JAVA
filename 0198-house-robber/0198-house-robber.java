58fc21b78942d11b8d47e127fe8ba4044751d5f0
Anay Verma <127543533+Anayverma@users.noreply.github.com>
2024-09-28 16:01:21 +0530

    Time: 0 ms (100%), Space: 41.4 MB (9.79%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return helper(nums, dp, nums.length - 1);
+    }
+
+    public int helper(int[] nums, int[] dp, int n) {
+        if (n < 0) return 0; 
+        if (dp[n] != -1) return dp[n]; 
+        int robCurrent = nums[n] + helper(nums, dp, n - 2); 
+        int skipCurrent = helper(nums, dp, n - 1);          
+        
+        dp[n] = Math.max(robCurrent, skipCurrent);
+        return dp[n];  
+    }
+}
