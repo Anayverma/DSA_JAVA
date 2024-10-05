ea6b14904808db7efc554de563e33b775232b6b3
Anay Verma <127543533+Anayverma@users.noreply.github.com>
2024-10-05 20:52:33 +0530

    Time: 7 ms (11.66%), Space: 45.2 MB (8.19%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,48 @@
+import java.io.*;
+class Solution
+{
+    public int romanToInt(String s)
+    {
+        int sum=0;
+        int n[]=new int[s.length()+1];
+       for(int i=s.length()-1;i>=0;i--)
+        {
+            switch(s.charAt(i))
+            {
+                case 'M':
+                n[i]=1000;
+                break;
+                case 'D':
+                n[i]=500;
+                break;
+                case 'C':
+                n[i]=100;
+                break;
+                case 'L':
+                n[i]=50;
+                break;
+                case 'X':
+                n[i]=10;
+                break;
+                case 'V':
+                n[i]=5;
+                break;
+                case 'I':
+                n[i]=1;
+                break;
+            }
+            if(n[i]<n[i+1])
+            n[i]*=-1;
+            
+            sum+=n[i];
+        }
+
+        return sum;
+   }
+   public static void main()throws IOException
+   {
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       Solution ob=new Solution();
+       System.out.println(ob.romanToInt(br.readLine()));
+   }
+}
